b9b777855f51f8c2db62e094c9fe4863b7052c2d
Michal Kurzal <michalkurzal200@gmail.com>
2021-10-20 23:37:59 +0200

    new cards components

---
+++ src/components/cards.js
@@ -0,0 +1,21 @@
+import MediaCard from "./card";
+import Grid from "@mui/material/Grid";
+
+export default function Cards() {
+  return (
+    <Grid
+      container
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-around",
+        alignItems: "center",
+        paddingTop: 10,
+      }}
+    >
+      <MediaCard />
+      <MediaCard />
+      <MediaCard />
+    </Grid>
+  );
+}
--- src/components/startPage.js
+++ src/components/startPage.js
@@ -2,7 +2,7 @@ import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import { styled } from "@mui/material/styles";
 import Button from "@mui/material/Button";
-import Box from "@mui/material/Box";
+import Cards from "./cards";
 
 export default function StartPage() {
   const style = {
@@ -17,36 +17,38 @@ export default function StartPage() {
   });
 
   return (
-    <Grid
-      container
-      spacing={2}
-      direction="row"
-      sx={{ paddingTop: 4, paddingLeft: 2 }}
-    >
-      <Grid item xs={3} md={6}>
-        <Img alt="complex" src="/desert.jpg" />
-      </Grid>
-      <Grid container spacing={2} xs={9} md={6}>
-        <Grid
-          item
-          xs
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Typography variant="h2" sx={style}>
-            Heading
-          </Typography>
-          <Typography sx={style} variant="h4">
-            Loren dihsdius dsiudhgsui dsuidgauih fdduifhgs dsudhgskj sduhsjkdh
-            Loren dihsdius dsiudhgsui dsuidgauih fdduifhgs dsudhgskj sduhsjkdh
-          </Typography>
-          <Button variant="contained">Contained</Button>
+    <Grid container>
+      <Grid
+        container
+        spacing={2}
+        sx={{ paddingTop: 4, paddingLeft: 2, paddingRight: 2 }}
+      >
+        <Grid item xs={12} md={6}>
+          <Img alt="complex" src="/desert.jpg" />
+        </Grid>
+        <Grid container spacing={2} xs={12} md={6}>
+          <Grid
+            item
+            xs
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Typography variant="h2" sx={style}>
+              Heading
+            </Typography>
+            <Typography sx={style} variant="h4">
+              Loren dihsdius dsiudhgsui dsuidgauih fdduifhgs dsudhgskj sduhsjkdh
+              Loren dihsdius dsiudhgsui dsuidgauih fdduifhgs dsudhgskj sduhsjkdh
+            </Typography>
+            <Button variant="contained">Contained</Button>
+          </Grid>
         </Grid>
       </Grid>
+      <Cards />
     </Grid>
   );
 }
