b18116231152dd0a23070cf3ba63f660bbc7ac93
Michal Kurzal <michalkurzal200@gmail.com>
2021-10-25 16:41:08 +0200

    footer grid

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,20 +1,17 @@
-import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
 
 export default function Footer() {
   return (
-    <Box
-      sx={{
-        width: "100%",
-        height: 100,
-        bgcolor: "primary.dark",
-        display: "flex",
-        justifyContent: "space-around",
-        alignItems: "center",
-      }}
-    >
-      <Typography variant="h5">Michal Kurzal 2021</Typography>
-      <Typography variant="h5">E-Mail: michalkurzal200@gmail.com</Typography>
-    </Box>
+    <Grid container sx={{ bgcolor: "primary.main", padding: 2 }}>
+      <Grid item xs={12} md={6} textAlign="center">
+        <Typography variant="h5">Michal Kurzal 2021</Typography>
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <Typography variant="h5" textAlign="center">
+          E-Mail: michalkurzal200@gmail.com
+        </Typography>
+      </Grid>
+    </Grid>
   );
 }
